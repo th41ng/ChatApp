06fe47bd20028795eee6a6599cf53ef9c61446b2
mthduy <mthduy7@gmail.com>
2024-11-01 23:36:31 +0700

    CodeDangKyDangNhap

---
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.example.chatapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailedit, passedit;
+    private Button btnLogin, btnRegister;
+    private FirebaseAuth mAuth;
+    private CheckBox chkRememberPass;
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailedit = findViewById(R.id.email);
+        passedit = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        chkRememberPass = findViewById(R.id.chk_RememberPass);
+
+        // Khởi tạo SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_FILE.xml", MODE_PRIVATE);
+
+        // Lấy trạng thái checkbox và dữ liệu đăng nhập nếu "Remember me" đã được chọn
+        boolean isRemembered = sharedPreferences.getBoolean("REMEMBER", false);
+        chkRememberPass.setChecked(isRemembered); // Set trạng thái của checkbox
+
+        if (isRemembered) {
+            String savedEmail = sharedPreferences.getString("EMAIL", "");
+            String savedPassword = sharedPreferences.getString("PASSWORD", "");
+            emailedit.setText(savedEmail);
+            passedit.setText(savedPassword);
+
+            // Tự động đăng nhập nếu đã lưu thông tin
+            login(savedEmail, savedPassword);
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailedit.getText().toString();
+                String pass = passedit.getText().toString();
+                login(email, pass);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    private void login(String email, String pass) {
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Vui lòng nhập email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Vui lòng nhập password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    rememberUser(email, pass, chkRememberPass.isChecked()); // Lưu trạng thái đăng nhập nếu chọn ghi nhớ
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Đăng nhập không thành công!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Phương thức lưu thông tin người dùng vào SharedPreferences
+    public void rememberUser(String email, String pass, boolean rememberStatus) {
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_FILE.xml", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if (rememberStatus) {
+            editor.putString("EMAIL", email);
+            editor.putString("PASSWORD", pass);
+            editor.putBoolean("REMEMBER", true); // Lưu trạng thái của checkbox
+            editor.putBoolean("isLoggedIn", true); // Đặt trạng thái là đã đăng nhập
+            Log.d(TAG, "Remember status saved");
+        } else {
+            editor.remove("EMAIL"); // Xóa email nếu không chọn ghi nhớ
+            editor.remove("PASSWORD"); // Xóa password nếu không chọn ghi nhớ
+            editor.putBoolean("REMEMBER", false); // Cập nhật trạng thái của checkbox
+            editor.putBoolean("isLoggedIn", false); // Cập nhật trạng thái là chưa đăng nhập
+            Log.d(TAG, "Remember status cleared");
+        }
+        editor.apply(); // Áp dụng các thay đổi
+    }
+
+}
---
+++ app/src/main/java/com/example/chatapp/RegisterActivity.java
@@ -0,0 +1,131 @@
+package com.example.chatapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mname, emailedit, passedit, mphone;
+    private FirebaseFirestore db;
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+    private static final String TAG = "RegisterActivity";
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+        db = FirebaseFirestore.getInstance();
+
+        emailedit = findViewById(R.id.email);
+        mname = findViewById(R.id.fullname);
+        passedit = findViewById(R.id.password);
+        mphone = findViewById(R.id.phone);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String email = emailedit.getText().toString();
+        String pass = passedit.getText().toString();
+        String name = mname.getText().toString();
+        String phone = mphone.getText().toString();
+
+        // Kiểm tra các trường nhập liệu
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Vui lòng nhập email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Vui lòng nhập mật khẩu!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Hiển thị ProgressBar
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // Ẩn ProgressBar
+                progressBar.setVisibility(View.GONE);
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+                    saveUserToFirestore(name, email, phone);
+                } else {
+                    // Ghi lại thông báo lỗi
+                    Log.e(TAG, "Đăng ký thất bại: " + task.getException().getMessage());
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản không thành công: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void saveUserToFirestore(String name, String email, String phone) {
+        String userId = mAuth.getCurrentUser() != null ? mAuth.getCurrentUser().getUid() : null;
+        if (userId == null) {
+            Toast.makeText(getApplicationContext(), "Không thể xác định người dùng", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Tạo một Map để lưu thông tin người dùng
+        HashMap<String, Object> user = new HashMap<>();
+        user.put("name", name);
+        user.put("email", email);
+        user.put("phone", phone);
+
+        // Thêm dữ liệu người dùng vào Firestore
+        db.collection("users").document(userId).set(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Thông tin người dùng đã được lưu", Toast.LENGTH_SHORT).show();
+                            // Chuyển đến MainActivity sau khi lưu thành công
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); // Kết thúc hoạt động này
+                        } else {
+                            Log.e(TAG, "Không thể lưu thông tin người dùng: " + task.getException().getMessage());
+                            Toast.makeText(getApplicationContext(), "Không thể lưu thông tin người dùng: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
